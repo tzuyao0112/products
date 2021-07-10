5f98269de62aefe77572f3d6c73a5bccb03464c4
Patrick Lin <tzuyao0112@gmail.com>
2021-07-10 14:39:37 +0800

    add check file excist

--- products.py
+++ products.py
@@ -1,12 +1,19 @@
+import os #operating system
+
 #讀取檔案
 products = []
-with open('products.csv', 'r', encoding='utf-8') as f:
-	for line in f:
-		if '商品,價格' in line:
-			continue
-		name, price = line.strip().split(',')
-		products.append([name, price])
-print(products)
+if os.path.isfile('products.csv'):
+	print('有已知檔案。')
+	with open('products.csv', 'r', encoding='utf-8') as f:
+		for line in f:
+			if '商品,價格' in line:
+				continue
+			name, price = line.strip().split(',')
+			products.append([name, price])
+	print(products)
+else:
+	print('查無此擋案。')
+
 
 #讓使用者輸入
 while True:
